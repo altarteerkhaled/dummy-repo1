bf5ee2be3d04b118fac547349500246630c95aab
altarteerkhaled <altarteer.khaled@gmail.com>
2025-01-16 01:52:34 +0300

    no

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         // edit feat1 branch - another one
     }
-    public static double add(double a, double b) {
-        return a + b;
+    public static double add(double a, doubkle b) {
+        return a - b;
     }
-    // testing the new comm
+    // tsting the new comm
 }
